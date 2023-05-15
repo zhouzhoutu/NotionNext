ae2ff5b40ad730f3d79195cb1c77273eceb82efc
2357 <76297759@qq.com>
2023-05-15 13:01:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '粥粥兔', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '博客是人生的后视镜，我愿记录我的一生。', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '博客是人生的后视镜。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://2357.life', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,软件应用,分享,投资理财,人生', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
