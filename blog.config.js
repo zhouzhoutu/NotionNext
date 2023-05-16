6dd51f17c8bf1e1eb7f89a1da2c3d9b03c23561b
2hou2hou <congeez@gmx.com>
2023-05-16 15:27:42 +0800

    change

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '粥粥', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '博客是人生的后视镜。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://2357.life', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,内容推荐,软件应用,自我提升,技术分享,生活记录,投资理财,人生感悟', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion,博客,内容推荐,软件应用,自我提升,技术分享,生活记录,投资理财,人生感悟', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'congeez@gmx.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
