c66f39dc426d2331f0dc213940f7006a842874ac
2357 <congeez@gmx.com>
2023-05-16 09:43:13 +0800

    调整next主题配置

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '粥粥兔', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '粥粥', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '博客是人生的后视镜。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://2357.life', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,内容推荐,软件应用,自我提升,技术分享,生活记录,投资理财,人生感悟', // 网站关键词 英文逗号隔开
